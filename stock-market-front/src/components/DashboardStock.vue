e31a548569d49da241573bbffa5b2b6c5370f206
emanuel <cardgameheritage@gmail.com>
2020-05-06 20:04:42 -0300

    trabalhando nos componentes

---
+++ stock-market-front/src/components/DashboardStock.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-text-field
+      v-model="firststockName"
+      :rules="stockNameRules"
+      :counter="10"
+      label="Enter a stock name to search"
+      required
+    ></v-text-field>
+    <DashboardTable />
+  </v-container>
+</template>
+
+<script>
+  import DashboardTable from './DashboardTable'
+
+  export default {
+    name: 'DashboardStock',
+    components: {
+      DashboardTable
+    },
+    data(){
+      return {
+        firststockName: '',
+        stockNameRules: [
+          v => !!v || 'Name is required',
+          v => v.length <= 10 || 'Name must be less than 10 characters',
+        ],
+      }
+    }
+  }
+</script>
---
+++ stock-market-front/src/components/DashboardTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-list two-line subheader>
+      <v-subheader>Listed stocks</v-subheader>
+      <v-list-item @click="console.log('clicked')">
+        <v-list-item-content>
+          <v-list-item-title>BA</v-list-item-title>
+          <v-list-item-subtitle>The Boeing Company</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data(){
+        return {
+
+        }
+    }
+  }
+</script>
